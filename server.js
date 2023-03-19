9dbc60672868c8b9e8c1a630b94b7f069b94fe30
James Kelly <jk377y@gmail.com>
2023-03-19 00:37:41 -0500

    added helpers middleware back to server.js

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({  });
 const sess = {
-	secret: 'Super secret secret',
+	secret: 'Super cool secret',
+	cookie: {},
 	resave: false,
 	saveUninitialized: true,
 	store: new SequelizeStore({
-		db: sequelize,
-	}),
+		db: sequelize
+	})
 };
 
-app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`));
+	app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}.`));  // formatted to make link clickable in VS Code console
 });
