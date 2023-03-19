3654901fd8f759b15e2f6787c4055efe72f2623b
James Kelly <jk377y@gmail.com>
2023-03-18 22:31:50 -0500

    added images and addComment edited

--- public/js/addComment.js
+++ public/js/addComment.js
@@ -1,30 +1,23 @@
+// This file is used to add a comment to a post by identifying the post id and inserting the comment to the database.
 async function commentFormHandler(event) {
-    event.preventDefault();
-  
-    const comment_text = document.querySelector('textarea[name='comment-body']').value.trim();
-    const post_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-  
-    if (comment_text) {
-      const response = await fetch('/api/comments', {
-        method: 'POST',
-        body: JSON.stringify({
-          post_id,
-          comment_text
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-  
-  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+	event.preventDefault();
+	const comment_text = document.querySelector('textarea[name="newComment"]').value.trim();
+	const post_id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
+	if (comment_text) {
+		const response = await fetch('/api/comments', {
+			method: 'POST',
+			body: JSON.stringify({ post_id, comment_text }),
+			headers: { 'Content-Type': 'application/json' }
+		});
+		if (response.ok) {
+			document.location.reload();
+		} else {
+			alert(response.statusText);
+		}
+	}
+}
+
+document.querySelector('#newComment').addEventListener('click', commentFormHandler);
   
