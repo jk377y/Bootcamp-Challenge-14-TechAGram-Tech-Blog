db64fe42641773e56db387dfa7bd4f9edbb353ce
James Kelly <jk377y@gmail.com>
2023-03-18 19:25:09 -0500

    updated dashboardRoutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,102 +1,98 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-//! for all http://localhost:3001/dashboard routes
+//! http://localhost:3001/dashboard routes
 
-// get all posts for dashboard
-router.get('/', (req, res) => {
-	console.log(' https://localhost:3001/dashboard GET ALL') // this is the route that is being hit when the getting data from all posts w/comment and user data for the dashboard
+// GET- get all posts for the dashboard
+router.get('/', withAuth, (req, res) => {
 	Post.findAll({
-		where: { user_id: req.session.user_id },
-		attributes: ['id', 'title', 'post_content', 'createdAt'],
-		include: [
-			{
-				model: Comment,
-				attributes: ['id', 'comment_content', 'post_id', 'user_id', 'createdAt'],
-				include: { model: User, attributes: ['username'] },
-			},
-			{
-				model: User,
-				attributes: ['username'],
-			},
-		],
-	})
-		.then((dbPostData) => {
-			const posts = dbPostData.map((post) => post.get({ plain: true }));
-			res.render('dashboard', { posts, loggedIn: true });
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
-});
-
-// GET page to edit a post
-router.get('/editPost/:id', (req, res) => {
-	console.log('https://localhost:3001/dashboard/editPost/:id GET ONE') // this is the route that is being hit when getting the edit post form
-	Post.findOne({
-		where: { id: req.params.id },
-		attributes: ['id', 'title', 'post_content', 'createdAt'],
-		include: [
-			{
-				model: Comment,
-				attributes: ['id', 'comment_content', 'post_id', 'user_id', 'createdAt'],
-				include: {
-					model: User,
-					attributes: ['username'],
-				},
-			},
-			{
-				model: User,
-				attributes: ['username'],
-			},
-		],
-	})
-		.then((dbPostData) => {
-			if (!dbPostData) {
-				res.status(404).json({ message: 'No post found with this id' });
-				return;
-			}
-			const post = dbPostData.get({ plain: true });
-			res.render('editPost', {
-				post,
-				loggedIn: req.session.loggedIn,
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
-});
-
-// GET page to create a new post 
-router.get('/newPost', (req, res) => {
-	console.log('https://localhost:3001/dashboard/newPost GET ONE') // this is the route that is being hit when the getting the new post form
-	Post.findAll({ where: {	user_id: req.session.user_id },
-		attributes: ['id', 'title', 'post_content', 'createdAt'],
-	  include: [
-		{
-		  model: Comment,
-		  attributes: ['id', 'comment_content', 'post_id', 'user_id', 'createdAt'],
-		  include: {
-			model: User,
-			attributes: ['username']
-		  }
+		where: {
+			user_id: req.session.user_id
 		},
-		{
-		  model: User,
-		  attributes: ['username']
-		}
-	  ]
+		attributes: [ 'id', 'post_content', 'title', 'created_at' ],
+		include: [
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+				include: { model: User, attributes: ['username'] }
+			},
+			{
+				model: User,
+				attributes: ['username']
+			}
+		]
 	})
-	  .then(dbPostData => {
+	.then(dbPostData => {
 		const posts = dbPostData.map(post => post.get({ plain: true }));
-		res.render('newPost', { posts, loggedIn: true });
-	  })
-	  .catch(err => {
+		res.render('dashboard', { 
+			posts, 
+			username: req.session.username, 
+			loggedIn: true });
+	})
+	.catch(err => {
 		console.log(err);
 		res.status(500).json(err);
-	  });
 	});
-  
-  module.exports = router;
+});
+
+// GET- route to edit post by selecting the id
+router.get('/edit/:id', withAuth, (req, res) => {
+	Post.findOne({
+		where: { id: req.params.id },
+		attributes: [ 'id', 'post_content', 'title', 'created_at' ],
+		include: [
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+				include: { 	model: User, attributes: ['username'] }
+			},
+			{
+				model: User,
+				attributes: ['username']
+			}
+		]
+	})
+	.then(dbPostData => {
+		if (!dbPostData) {
+			res.status(404).json({ message: 'No post found with this id' });
+			return;
+		}
+		const post = dbPostData.get({ plain: true });
+		res.render('editPost', { post, loggedIn: req.session.loggedIn });
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json(err);
+	});
+});
+
+// GET- to route to the addPost page 
+router.get('/addPost', withAuth, (req, res) => {
+	Post.findAll({
+		where: { user_id: req.session.user_id },
+		attributes: [ 'id', 'post_content', 'title', 'created_at' ],
+		include: [
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+				include: { model: User, attributes: ['username'] }
+			},
+			{
+				model: User,
+				attributes: ['username']
+			}
+		]
+	})
+	.then(dbPostData => {
+		const posts = dbPostData.map(post => post.get({ plain: true }));
+		res.render('addPost', { posts, loggedIn: true });
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json(err);
+	});
+});
+
+module.exports = router;
