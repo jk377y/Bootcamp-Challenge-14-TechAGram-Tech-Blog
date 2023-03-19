2bb683d28d1be0e92625b03fc3576c63a38ff5f9
James Kelly <jk377y@gmail.com>
2023-03-18 19:16:54 -0500

    updated connection.js and schema files, added comments

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,6 @@
-// this file is used to connect to the database
+// This file is used to connect to the database using Sequelize and dotenv packages	to hide the database credentials.
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
-
-// create connection to the db and pass in MySQL information for username and password 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
--- Clears old database and creates a new one
-DROP DATABASE IF EXISTS techagram_db;
-CREATE DATABASE techagram_db;
-
+-- dropping database if exists
+DROP DATABASE IF EXISTS techAgram_db;
+-- creating database
+CREATE DATABASE techAgram_db;
+-- using database
 USE techAgram_db;
