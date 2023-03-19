38fac348a1f7f47f5525000f25a86b77f162ebfc
James Kelly <jk377y@gmail.com>
2023-03-18 22:36:10 -0500

    minor changes to deletePost

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,18 +1,18 @@
+// identify the post id and insert it into the delete route in the html
+// then a fetch request is made to the delete route in the api
 async function deleteFormHandler(event) {
-    event.preventDefault();
-  
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE'
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-  
-  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+	event.preventDefault();
+	const id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
+	const response = await fetch(`/api/posts/${id}`, {
+		method: 'DELETE'
+	});
+	if (response.ok) {
+		document.location.replace('/dashboard/');
+	} else {
+		alert('ALL comments must be deleted before deleting the post is allowed.');
+	}
+}
+
+document.querySelector('#deletePostBtn').addEventListener('click', deleteFormHandler);
