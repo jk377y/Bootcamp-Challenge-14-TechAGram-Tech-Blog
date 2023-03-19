5f9876f63c1f48f513f8f2c5b0539c462022dd02
James Kelly <jk377y@gmail.com>
2023-03-19 01:20:07 -0500

    editing db connection due to heroku issues

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const sess = {
 	secret: 'Super cool secret',
-	cookie: {},
+	cookie: {maxAge: 30 * 60 * 1000},
 	resave: false,
 	saveUninitialized: true,
 	store: new SequelizeStore({
