50cf8b06a1d6fcb5dc498253b433b2b0b6831b8e
James Kelly <jk377y@gmail.com>
2023-03-18 19:27:55 -0500

    updated comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,44 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Comment extends Model {}
 
-// creates the Comment model
 Comment.init(
-  {
-    id: {  // setting up properties for the Comment model
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    comment_content: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',  // references the User model's id property as the foreign key
-        key: 'id',
-      },
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'post',  // references the Post model's id property as the foreign key
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		comment_text: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1],
+			},
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "user",
+				key: "id",
+			},
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "post",
+				key: "id",
+			},
+		},
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "comment",
+	}
 );
 
 module.exports = Comment;
