e4c0af9ed38c044c902a0305807d4d6e35240015
James Kelly <jk377y@gmail.com>
2023-06-18 15:31:25 -0500

    updating

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const sess = {
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
